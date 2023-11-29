dc7ad45ff367e059f7c026668b1d6c99c3970413
Weslei França <weslwifranca@gmail.com>
2023-11-29 19:39:58 -0300

    delete dotenv app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,7 @@ import express from 'express'
 import routes from './routes'
 
 import './database'
-
 import cors from 'cors'
-dotenv.config()
-
 class App {
   constructor() {
     this.app = express()
